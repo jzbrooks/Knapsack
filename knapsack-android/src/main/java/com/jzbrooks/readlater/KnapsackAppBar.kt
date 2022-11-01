b0fac09f2681a3d875c4c936fe6b2cc0c2b3003b
Justin Brooks <justin@jzbrooks.com>
2022-10-31 20:09:11 -0500

    Show a back button on the app bar

---
+++ knapsack-android/src/main/java/com/jzbrooks/readlater/KnapsackAppBar.kt
@@ -0,0 +1,40 @@
+package com.jzbrooks.readlater
+
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@Composable
+fun KnapsackAppBar(navController: NavController) {
+    val currentBackStackEntry = navController
+        .currentBackStackEntryFlow
+        .collectAsState(initial = navController.currentBackStackEntry)
+
+    val currentRoute = currentBackStackEntry.value?.destination?.route
+    val showBack = currentRoute !in listOf(Routes.HOME, Routes.AUTH)
+
+    if (currentRoute == Routes.AUTH) return
+
+    TopAppBar(
+        title = { Text(stringResource(R.string.app_name)) },
+        backgroundColor = MaterialTheme.colors.primaryVariant,
+        elevation = 0.dp,
+        navigationIcon = if (showBack) {
+            {
+                IconButton(onClick = { navController.navigateUp() }) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "Back"
+                    )
+                }
+            }
+        } else {
+            null
+        }
+    )
+}
