23ae43d8673a32c3d8cef5ab4c4a29429a25849e
Justin Brooks <justin@jzbrooks.com>
2022-11-01 21:34:38 -0500

    Fix double use of refresh token

--- knapsack-android/src/main/java/com/jzbrooks/readlater/ReadingScreen.kt
+++ knapsack-android/src/main/java/com/jzbrooks/readlater/ReadingScreen.kt
@@ -1,26 +1,31 @@
 package com.jzbrooks.readlater
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.jsoup.Jsoup
 import org.jsoup.select.NodeTraversor
 
 @Composable
 fun ReadingScreen(content: String) {
-    val test = FormattingVisitor()
-    NodeTraversor.traverse(test, Jsoup.parse(content).root())
+    val basicFormatter = FormattingVisitor()
+    NodeTraversor.traverse(basicFormatter, Jsoup.parse(content).root())
+
     Text(
-        test.toString(),
-        modifier = Modifier.verticalScroll(rememberScrollState()),
+        basicFormatter.toString(),
+        modifier = Modifier
+            .padding(start = 16.dp, top = 0.dp, end = 16.dp, bottom = 16.dp)
+            .verticalScroll(rememberScrollState()),
     )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun Preview() {
-    ReadingScreen("https://logos.com")
+    ReadingScreen("<p>This is <b>big</b></p>")
 }
