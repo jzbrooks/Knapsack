4e8365c5538e5262f65fdaf4f7642644cafc6e10
Justin Brooks <justin@jzbrooks.com>
2022-11-19 19:38:48 -0600

    Fix a lint issue

--- knapsack-android/src/main/java/com/jzbrooks/readlater/ReadingScreen.kt
+++ knapsack-android/src/main/java/com/jzbrooks/readlater/ReadingScreen.kt
@@ -10,15 +10,9 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.os.bundleOf
 import androidx.navigation.NavBackStackEntry
-import androidx.navigation.NavDestination
-import com.jzbrooks.readlater.data.Entry
 import com.jzbrooks.readlater.data.EntryRepository
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import org.jsoup.Jsoup
 import org.jsoup.select.NodeTraversor
 
@@ -44,38 +38,4 @@ fun ReadingScreen(backStackEntry: NavBackStackEntry, repository: EntryRepository
             .padding(start = 16.dp, top = 0.dp, end = 16.dp, bottom = 16.dp)
             .verticalScroll(rememberScrollState()),
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun Preview() {
-    val entry = NavBackStackEntry.create(
-        null,
-        NavDestination("dummy"),
-        arguments = bundleOf("id" to 1)
-    )
-
-    val repository = object : EntryRepository {
-        val article = Entry(
-            1,
-            "example",
-            "https://example.com",
-            "<p>This is <b>big</b></p>",
-            null,
-        )
-
-        override val entries: Flow<List<Entry>>
-            get() = flow {
-                emit(listOf(article))
-            }
-
-        override suspend fun updateEntries() { }
-        override suspend fun deleteAllEntries() { }
-
-        override suspend fun getEntry(id: Long): Entry? {
-            return if (id == 1L) article else null
-        }
-    }
-
-    ReadingScreen(entry, repository)
 }
