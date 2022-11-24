794f2f587bc00692fa15206c909de85518a4192b
Justin Brooks <justin@jzbrooks.com>
2022-11-23 21:20:00 -0600

    Tinker with dom parsing pods

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,24 +1,30 @@
-import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
+import org.jetbrains.kotlin.gradle.plugin.mpp.BitcodeEmbeddingMode.BITCODE
 
 plugins {
     id("org.jetbrains.kotlin.multiplatform")
     id("org.jetbrains.kotlin.plugin.serialization")
+    id("org.jetbrains.kotlin.native.cocoapods")
     id("com.android.library")
     id("com.squareup.sqldelight")
 }
 
 kotlin {
     android()
+    ios()
 
-    val xcf = XCFramework()
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64(),
-    ).forEach {
-        it.binaries.framework {
+    cocoapods {
+        version = "1.0"
+        summary = "Some description for a Kotlin/Native module"
+        homepage = "Link to a Kotlin/Native module homepage"
+
+        framework {
             baseName = "data"
-            xcf.add(this)
+            ios.deploymentTarget = "15"
+        }
+
+        pod("GONMarkupParser")
+        pod("GDataXML-HTML") {
+            version = "~> 1.4.1"
         }
     }
 
@@ -55,28 +61,15 @@ kotlin {
         }
 
         val androidTest by getting
-        val iosX64Main by getting
-        val iosArm64Main by getting
-        val iosSimulatorArm64Main by getting
-        val iosMain by creating {
+        val iosMain by getting {
             dependsOn(commonMain)
-            iosX64Main.dependsOn(this)
-            iosArm64Main.dependsOn(this)
-            iosSimulatorArm64Main.dependsOn(this)
-
             dependencies {
                 implementation("com.squareup.sqldelight:native-driver:1.5.3")
                 implementation("io.ktor:ktor-client-ios:2.1.2")
             }
         }
-        val iosX64Test by getting
-        val iosArm64Test by getting
-        val iosSimulatorArm64Test by getting
-        val iosTest by creating {
+        val iosTest by getting {
             dependsOn(commonTest)
-            iosX64Test.dependsOn(this)
-            iosArm64Test.dependsOn(this)
-            iosSimulatorArm64Test.dependsOn(this)
         }
     }
 }
--- data/src/iosMain/kotlin/com/jzbrooks/readlater/data/text/HtmlString.kt
+++ data/src/iosMain/kotlin/com/jzbrooks/readlater/data/text/HtmlString.kt
@@ -1,6 +1,22 @@
 package com.jzbrooks.readlater.data.text
 
+import cocoapods.GDataXML_HTML.*
+import cocoapods.GONMarkupParser.*
+import kotlinx.cinterop.utf8
+import platform.Foundation.NSAttributedString
+import platform.Foundation.NSAttributedStringEnumerationOptions
+import platform.Foundation.NSAttributedStringEnumerationReverse
+import platform.Foundation.NSData
+import platform.Foundation.NSRangeFromString
+import platform.Foundation.NSUTF16StringEncoding
+import platform.Foundation.enumerateAttributesInRange
+import platform.Foundation.length
+
 // todo: parse html with swiftsoup?
 fun HtmlString.toStyledString(): StyledString {
+    val string = GONMarkupParser.defaultMarkupParser()?.attributedStringFromString(toString())
+    string!!.enumerateAttributesInRange(NSRangeFromString(string.toString()), 0) { a, b, c ->
+    }
+
     return StyledString(toString())
 }
